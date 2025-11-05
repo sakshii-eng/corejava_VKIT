b2d3d02792fb7b0a3a35f3fe266f33c870fa2b32
Sakshi <Sakshi@LAPTOP-TLS128MC>
2025-11-05 11:52:12 +0530

    first

---
+++ demojavaprograming/src/com/tnsif/Construtorprogram/Conexample.java
@@ -0,0 +1,22 @@
+package com.tnsif.Construtorprogram;
+// demo construtor
+public class Conexample {
+	Conexample(int i){
+		System.out.println("constructor with 1 parameter");
+	}
+	
+	Conexample(int i,int j){
+		System.out.println("constructor with 2 parameter");
+	}
+	Conexample(int i, String g , int w){
+		System.out.println("constructor with 3 parameter");
+	}
+	public static void main(String[] args) {
+		//Conexample c=new Conexample(9,"gfhg");
+		Conexample c1=new Conexample(7,9);
+		Conexample c2=new Conexample(5);
+		Conexample c3=new Conexample(7,"rash",9);
+		
+	}
+
+}
---
+++ demojavaprograming/src/com/tnsif/Construtorprogram/Student.java
@@ -0,0 +1,13 @@
+package com.tnsif.Construtorprogram;
+//this key word
+public class Student {
+int rollno;
+String name;
+
+//paramater construtor
+public Student(int rollno, String name) {
+	this.rollno = rollno;
+	this.name = name;
+}
+
+}
